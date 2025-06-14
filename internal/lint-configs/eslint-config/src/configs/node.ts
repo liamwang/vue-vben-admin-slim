7487a36042920ca14bc7f71e6b76534e69487181
Liam <liam.wang@live.com>
2025-06-14 12:13:30 +0800

    移除vitest配置

--- internal/lint-configs/eslint-config/src/configs/node.ts
+++ internal/lint-configs/eslint-config/src/configs/node.ts
@@ -20,7 +20,6 @@ export async function node(): Promise<Linter.Config[]> {
             allowModules: [
               'unbuild',
               '@vben/vite-config',
-              'vitest',
               'vite',
               '@vue/test-utils',
               '@vben/tailwind-config',
